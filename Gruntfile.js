d5fee4e07ec56cb898dd2a6cc16fadc1db15bcd4
Charlotte Simonnet <missimlab@gmail.com>
2016-11-29 17:50:31 +0100

    Configured Grunt to combine and minify javascripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,15 +4,15 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      build: { 
+        files: [{
+            src: 'src/*.js',
+            dest: 'compiled/mislab-tagmanager.min.js'
+        }]
       }
     },
-    src_files: ['src/*.js']
+    src_files: ['src/*.js'],
+    dest_files: ['compiled/*.min.js']
   });
 
   // Load the plugin that provides the "uglify" task.
