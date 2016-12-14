b3fe3aacdd3b0f5574ea2240333dfca9c1ab7830
Charlotte Simonnet <missimlab@gmail.com>
2016-12-14 14:48:13 +0100

    Adding remove and add tags

--- src/mislab-tagmanager.class.js
+++ src/mislab-tagmanager.class.js
@@ -6,10 +6,13 @@ function MislabTagManager() {
     this.options = {
         "ajax_autocomplete": false,
     };
+    this.inputId = null;
 };
 
 MislabTagManager.prototype.init = function(input) {
-    this.domHandler = new MislabTagDOMHandler(input);
+    this.inputId = this.generateInputId();
+    this.input = input;
+    this.domHandler = new MislabTagDOMHandler(input, this.inputId);
     this.eventHandler = new MislabTagEventHandler(this, this.options);
     this.tags = this.domHandler.getTagsFromInput();
     this.domHandler.initDom(this.tags);
@@ -39,4 +42,41 @@ MislabTagManager.prototype.highlightTag = function(tag)
 MislabTagManager.prototype.unhighlightTag = function(tag)
 {
     this.domHandler.unhighlightTag(tag);
+}
+
+MislabTagManager.prototype.hideList = function()
+{
+    this.domHandler.hideList();
+}
+
+MislabTagManager.prototype.generateInputId = function() {
+    return Math.random().toString(36).substr(2, 9);
+}
+
+MislabTagManager.prototype.selectTag = function(tagLabel) {
+    var tag = this.getTagObjectByLabel(tagLabel);
+    this.domHandler.createTag(tag);
+    tag.selected = false;
+    this.domHandler.hideList();
+}
+
+MislabTagManager.prototype.getTagObjectByLabel = function(tagLabel) {
+    for(key in this.tags) {
+        if(this.tags[key].label == tagLabel) {
+            return this.tags[key];
+        }
+    }
+}
+
+MislabTagManager.prototype.removeTag = function(tagLabel) {
+    var tag = this.getTagObjectByLabel(tagLabel);
+    this.domHandler.removeTag(tag);
+    tag.selected = false;
+}
+
+
+MislabTagManager.prototype.enterTag = function(tagLabel) {
+    var tag = new MislabTagObject(tagLabel, true, this.inputId);
+    this.tags.push(tag);
+    this.domHandler.createTag(tag);
 }
